ae98500c0d91e13a2c70aa34f13e490aabf460da
soumya <soumya@example.com>
2025-02-04 22:31:11 +0530

    implements react component and render multiple elements

--- app.js
+++ app.js
@@ -1,47 +1,34 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 
-const container = (
-  <div className="container">
-    <div className="cards">
-      <div className="card">
-        <h2 style={{ color: "blue" }}>Study React</h2>
-        <img
-          src="https://www.curotec.com/wp-content/uploads/2024/10/top-uses-for-react-e1729004277386.jpg?w=1024"
-          style={{ width: "300px" }}
-        />
-        <p>
-          React is the library for web and native user interfaces. Build user
-          interfaces out of individual pieces called components written in
-          JavaScript.
-        </p>
-      </div>
-      <div className="card">
-        <h2 style={{ color: "red" }}>Learn React</h2>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbdCxfo8H5NcKMZcGw2VK9hEEBS2-oB63XpA&amp;s"
-          style={{ width: "300px" }}
-        />
-        <p>
-          React is a JavaScript library for building user interfaces. React is
-          used to build single-page applications. React allows us to create
-          reusable UI components.
-        </p>
-      </div>
+const root = createRoot(document.querySelector("#root"));
+function Card(pros) {
+  return (
+    <div className="card">
+      <h3>{pros.author}</h3>
+      <img src={pros.authorImage} style={{ width: "100%" }} />
+      <h3>{pros.category}</h3>
+      <p>{pros.quote}</p>
     </div>
-    <form>
-      <div className="user_group">
-        <label htmlFor="user">Username</label>
-        <input id="user" />
+  );
+}
+let list = [];
+fetch("https://dummy-json.mock.beeceptor.com/quotes")
+  .then((res) => res.json())
+  .then((result) => {
+    root.render(
+      <div className="cards">
+        {list = result.map((product) => {
+          return (
+            <Card
+              author={product.author}
+              authorImage={product.authorImage}
+              category={product.category}
+              quote={product.quote}
+            />
+          );
+        })}{console.log(list)}
       </div>
-      <div className="user_group">
-        <label htmlFor="pass">Password</label>
-        <input type="password" id="pass" />
-      </div>
-    </form>
-    <p>lorem</p>
-  </div>
-);
-
-console.log(container);
-var root = createRoot(document.querySelector("#root"));
-root.render(container);
+      
+    );
+  });
